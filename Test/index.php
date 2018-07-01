8ea64990d852dc511cf8eeacaf7a396fcc4cb261
oscarfabian <oscarfabian@gmail.com>
2018-07-01 18:46:23 -0400

    primer commit

---
+++ Test/index.php
@@ -0,0 +1,52 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<title></title>
+</head>
+<body>
+
+	<form action="rescatar.php" method="post">
+		<label> Ingrese nombre </label>
+		<br>
+       <input type="text" name="nombre" id="nombre" >
+	    <br>
+        
+       <label> Ingrese Apellido paterno </label>
+       <br>
+       
+       <input type="text" name="apaterno" id="apaterno" >
+        <br>   
+        <label> Ingrese Apellido materno </label>
+        <br>
+       <input type="text" name="amaterno" id="amaterno" >
+        <br>   
+        <label> Ingrese direccion </label>
+        <br>
+       <input type="text" name="direccion" id="direccion" >
+        <br>   
+        <label> Ingrese correo </label>
+        <br>
+       <input type="text" name="correo" id="correo" >
+         <br>  
+        <label> Ingrese Usuario </label>
+        <br>
+       <input type="text" name="usuario" id="usuario" >
+        <br>   
+       <label> Ingrese Clave </label>
+        <br>
+       <input type="password" name="clave" id="clave" >
+        <br> 
+       <input type="submit" id="btn_enviar" value="enviar">
+       <input type="button" id="btn_resetear"  value="resetear" onclick="vaciar()" >
+
+           
+           
+
+
+
+
+
+	</form>
+
+</body>
+</html>
---
+++ Test/js/funciones.js
@@ -0,0 +1,7 @@
+
+function agregar($variable){
+	
+var variable=document.getElementById('mensaje').value;
+return variable.push('cacahuate');
+
+} 
